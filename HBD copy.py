9270203e6f362613e486c7410c6735166a18d348
Simplexer <72856990+juho-creator@users.noreply.github.com>
2024-01-12 05:01:17 +0900

    Add files via upload

---
+++ HBD copy.py
@@ -0,0 +1,50 @@
+import subprocess
+import keyboard
+import time
+import pyautogui
+from textwrap import *
+import pyperclip
+from modules import *
+
+
+subprocess.call(["cmd", "/c", "start", "/max", "C:\\Windows\\notepad.exe"])
+time.sleep(1)
+
+
+typeout(text,0.001)
+
+time.sleep(1)  # Give some time to focus on the input field
+
+keyboard.write(poo)
+time.sleep(5)
+
+for i in range(12):
+  shift_text(poo)
+
+display(three)
+display(two)
+display(one)
+
+
+keyboard.write(big_cake)
+time.sleep(5)
+keyboard.write(congrats)
+time.sleep(5)
+
+keyboard.write(hbd)
+time.sleep(5)
+keyboard.write(cake)
+
+typeout(text2,0.001)
+typeout(breath_in,0.5)
+
+
+keyboard.write(breath_out)
+
+typeout(final,0.0001)
+
+   
+time.sleep(2)
+keyboard.write(respect)
+
+
